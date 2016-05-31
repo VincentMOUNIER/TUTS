57859a8806b362bea4aec9f1117339583956c1e6
KevinReikho <Reikonekoz@hotmail.com>
2016-05-31 13:57:04 +0200

    Revert "MAJ"
    This reverts commit 8d6ccb007591134135d74f5825ea2039e90c6c5f.

--- WordPress/wp-content/themes/css/functions.php
+++ WordPress/wp-content/themes/css/functions.php
@@ -93,7 +93,7 @@ function recupextension ($filename)
 
 
 
-function uploadLogo(){  //TODO Mettre une limitation de image size 
+function uploadLogo(){
 
   // Check that the nonce is valid, and the user can edit this post.
   if (
